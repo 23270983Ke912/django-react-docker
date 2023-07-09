d3c7954a5c781d2ca8e605bfa74624638b321fce
23270983Ke912 <23270983@student.uwa.edu.au>
2023-07-09 15:18:47 +0800

    initial commit

---
+++ backend/backend/views.py
@@ -0,0 +1,10 @@
+# django-react-docker/backend/backend/views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def send_some_data(request):
+    return Response({
+        "data": "Hello from django backend"
+    })
+    
